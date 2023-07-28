4499b2686868d79f713902ca913758b3ab3e41f1
EddieBreeg <eddiebreeg0@protonmail.com>
2023-07-27 22:35:25 -0400

    added doc + a few features

--- main.cpp
+++ main.cpp
@@ -3,21 +3,32 @@
 
 struct F {
 	bool _moved = false;
-	char _dummy[8];
+	int _val = 0;
 	F() = default;
 	F(const F &) = delete;
-	F(F &&other) { other._moved = true; }
+	F(F &&other) : _val(other._val) { other._moved = true; }
 	int operator()() const { return 10; }
 	~F() {
 		if (!_moved) std::cout << "Destroyed\n";
 	}
 };
 
-int f() {
-	return 10;
+F foo() {
+	F res{};
+	res._val = 10;
+	return res;
+}
+
+unique_function<F()> make_func() {
+	return [f = F{}]() mutable {
+		f._val = 11;
+		return std::move(f);
+	};
 }
 
 int main(int argc, char const *argv[]) {
-	unique_function<int()> func = F{};
-	std::cout << func() << '\n';
+	unique_function<F()> f = foo;
+	f = make_func();
+	F x = f();
+	std::cout << x._val << '\n';
 }
--- unique_function.hpp
+++ unique_function.hpp
@@ -1,6 +1,10 @@
 #if !defined(UNIQUE_FN_H)
 #define UNIQUE_FN_H
 
+#include <type_traits>
+#include <utility>
+#include <typeinfo>
+
 template <class T>
 constexpr T &&forward(std::remove_reference_t<T> &t) noexcept {
 	return static_cast<T &&>(t);
@@ -12,20 +16,43 @@ constexpr T &&forward(std::remove_reference_t<T> &&t) noexcept {
 // required in case we need to forward an empty argument pack
 constexpr void forward() noexcept {}
 
+// Exception thrown when the call operator is used on an invalid unique_function
+// object
 struct invalid_function_access {
 	constexpr const char *what() noexcept {
 		return "Attempted to call an invalid unique_function object";
 	}
 };
 
+/**
+ * Represents a generic non-copyable callable object
+ * @tparam Func: The underlying function type
+ */
 template <class Func> class unique_function;
 
 template <class R, typename... Args> class unique_function<R(Args...)> {
 public:
+	/* Default constructor: constructs an invalid function object */
 	constexpr unique_function() noexcept = default;
+
+	/**
+	 * Contructs a unique_function instance from a function pointer
+	 */
+	unique_function(R (*f)(Args...)) : _isSmall(true), _ptr(f) {
+		using F = R (*)(Args...);
+		_tid = &typeid(F);
+		_invoke = [](void *f, Args &&...args) {
+			return (*(F *)f)(forward<Args>(args)...);
+		};
+	}
+
+	/**
+	 * Contructs a unique_function instance from a generic callable object
+	 */
 	template <class F>
 	constexpr unique_function(F &&f) : _isSmall(sizeof(f) < 9) {
 		using _Raw = std::remove_reference_t<F>;
+		_tid = &typeid(_Raw);
 		if (_isSmall) new (_mem) _Raw(std::forward<_Raw>(f));
 		else _ptr = new _Raw(std::forward<_Raw>(f));
 		if (_isSmall && !std::is_trivially_constructible_v<_Raw>)
@@ -35,13 +62,13 @@ public:
 			return (*(_Raw *)f)(forward<Args>(args)...);
 		};
 	}
-	unique_function(R (*f)(Args...)) : _isSmall(true), _ptr(f) {
-		using F = R (*)(Args...);
-		_invoke = [](void *f, Args &&...args) {
-			return (*(F *)f)(forward<Args>(args)...);
-		};
-	}
 	unique_function(const unique_function &) = delete;
+
+	/**
+	 * Constructs a unique_function by moving another
+	 * @param other: The object to move from. This object is left in an invalid
+	 * state, such that a call to has_value() returns false
+	 */
 	constexpr unique_function(unique_function &&other) noexcept :
 		_ptr(other._ptr),
 		_isSmall(other._isSmall),
@@ -49,16 +76,65 @@ public:
 		_invoke(other._invoke) {
 		other.reset();
 	}
+	/**
+	 * Indicates whether *this is a valid function object
+	 * @returns true if *this contains a valid callable object, false otherwise
+	 */
+	constexpr bool has_value() const noexcept { return (bool)_invoke; }
+	/**
+	 * Casts *this to a boolean
+	 * @return Same as has_value()
+	 */
+	constexpr operator bool() const noexcept { return has_value(); }
+
+	/**
+	 * Swaps the contents of *this with other
+	 * @param other: The other function object to swap the contents with
+	 */
+	void swap(unique_function &other) {
+		std::swap(_ptr, other._ptr);
+		std::swap(_invoke, other._invoke);
+		std::swap(_deleter, other._deleter);
+		std::swap(_isSmall, other._isSmall);
+		std::swap(_tid, other._tid);
+	}
+	unique_function &operator=(unique_function &) = delete;
+	unique_function &operator=(unique_function &&other) {
+		swap(other);
+		return *this;
+	}
+	/**
+	 * Attempts to invoke the function
+	 * @param args: The arguments to forward to the underlying callable object
+	 * @throw Throws invalid_function_access if *this does not contain a valid
+	 * callable object, that is if has_value() returns false
+	 */
+	R operator()(Args &&...args) {
+		if (!_invoke) throw invalid_function_access{};
+		return _invoke(_isSmall ? _mem : _ptr, forward<Args>(args)...);
+	}
+	/**
+	 * @return typeid(T) if the stored function has type T, otherwise
+	 * typeid(void)
+	 */
+	const std::type_info &target_type() const noexcept {
+		return _tid ? *_tid : typeid(void);
+	}
+	/**
+	 * @returns A pointer to the stored function if target_type() == typeid(T),
+	 * otherwise a null pointer.
+	 */
+	template <class T> T *target() noexcept {
+		if (*_tid != typeid(T)) return nullptr;
+		return _isSmall ? (T *)_mem : (T *)_ptr;
+	}
+	/**
+	 * Destroys the function object
+	 */
 	~unique_function() {
 		if (!_deleter) return;
 		if (_isSmall) _deleter(_mem);
 		else _deleter(_ptr);
-	}
-	constexpr bool has_value() const noexcept { return _isSmall || (bool)_ptr; }
-	constexpr operator bool() const noexcept { return has_value(); }
-	R operator()(Args &&...args) {
-		if (!_invoke) throw invalid_function_access{};
-		return _invoke(_isSmall ? _mem : _ptr, forward<Args>(args)...);
 	}
 
 private:
@@ -75,6 +151,7 @@ private:
 	bool _isSmall = false;
 	void (*_deleter)(void *ptr) = nullptr;
 	R (*_invoke)(void *, Args &&...) = nullptr;
+	const std::type_info *_tid = nullptr;
 };
 
 #endif // UNIQUE_FN_H
