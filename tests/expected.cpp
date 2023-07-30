1c26a05ea8b270be8f9afd7cb651f8c997a1f0b6
EddieBreeg <eddiebreeg0@protonmail.com>
2023-07-30 11:22:17 -0400

    fixed tests for clang and msvc

--- tests/expected.cpp
+++ tests/expected.cpp
@@ -3,32 +3,22 @@
 #include <stdexcept>
 #include <iostream>
 
-struct dummy_error {
-	constexpr dummy_error() = default;
-	const char *what() const noexcept { return "Dummy Error"; }
-};
-struct error2 {
-	const char *_msg = nullptr;
-	constexpr error2(const char *str) noexcept : _msg(str) {}
-	constexpr error2(std::initializer_list<int>, const char *str) noexcept :
-		_msg(str) {}
-	constexpr const char *what() const noexcept { return _msg; }
-	constexpr bool operator==(const error2 &other) const {
-		return _msg == other._msg;
+struct E {
+	const int _code;
+	constexpr E(int c) noexcept : _code(c) {}
+	constexpr E(std::initializer_list<int>, int c) noexcept : _code(c) {}
+	constexpr int what() const noexcept { return _code; }
+	constexpr bool operator==(const E &other) const {
+		return _code == other._code;
 	}
 };
-constexpr bool operator==(const dummy_error &, const dummy_error &) {
-	return true;
-}
 
 void constexpr_tests() {
-	constexpr libstra::unexpected<dummy_error> x{ dummy_error{} };
-	static_assert(error2{ "Msg" }.what() == "Msg");
-	constexpr libstra::unexpected<error2> y(libstra::in_place_t{},
-											(const char *)"foobar");
-	constexpr decltype(y) z(libstra::in_place_t{}, { 1, 2 }, "foobar");
-	static_assert(z.error().what() == "foobar");
-	static_assert(y == z);
+	constexpr libstra::unexpected<E> a(E(42));
+	static_assert(a.error().what() == 42, "constexpr test failed");
+	constexpr libstra::unexpected<E> b(libstra::in_place_t{}, 1);
+	constexpr libstra::unexpected<E> c(libstra::in_place_t{}, { 1, 2, 3 }, 1);
+	static_assert(b == c, "constexpr test failed");
 }
 
 class Foo {
