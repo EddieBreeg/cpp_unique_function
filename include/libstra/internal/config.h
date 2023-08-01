dae6f6449999900451f844028743acdcd27ea16d
EddieBreeg <eddiebreeg0@protonmail.com>
2023-08-01 19:43:42 -0400

    added _assume definition for several compilers

--- include/libstra/internal/config.h
+++ include/libstra/internal/config.h
@@ -9,6 +9,13 @@
 
 #if __cplusplus >= 202302L
 #define _assume(expr) [[assume(expr)]]
+#elif defined(_MSC_VER) && !defined(__clang__)
+#define _assume(expr) __assume(expr)
+#elif defined(__clang__)
+#define _assume(expr) __builtin_assume(expr)
+#elif defined(__GNUC__)
+#define _assume(expr)                                                          \
+	if (!(expr)) __builtin_unreachable()
 #else
 #define _assume(exp)
 #endif
