387a8ac324e3739e08aeff8d4ae3607d24f2d29b
EddieBreeg <eddiebreeg0@protonmail.com>
2023-08-07 11:16:45 +0200

    implemented basic array_view class

---
+++ include/libstra/views.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <cstddef>
+
+namespace libstra {
+	template <class T>
+	class array_view {
+	private:
+		const T *_start = nullptr, *_end = nullptr;
+
+	public:
+		constexpr array_view() noexcept = default;
+		constexpr array_view(const T *ptr, size_t n) noexcept :
+			_start(ptr), _end(ptr + n) {}
+		template <class Iter>
+		constexpr array_view(Iter begin, Iter end) noexcept :
+			_start(&(*begin)), _end(&(*end)) {}
+
+		[[nodiscard]]
+		constexpr const T &
+		operator[](size_t i) const {
+			return _start[i];
+		}
+		[[nodiscard]]
+		constexpr const T *data() const noexcept {
+			return _start;
+		}
+		[[nodiscard]]
+		constexpr const T &front() const {
+			return *_start;
+		}
+		[[nodiscard]]
+		constexpr const T &back() const {
+			return *(_end - 1);
+		}
+		[[nodiscard]]
+		constexpr const T *begin() const noexcept {
+			return _start;
+		}
+		[[nodiscard]]
+		constexpr const T *end() const noexcept {
+			return _end;
+		}
+		[[nodiscard]]
+		constexpr size_t size() const noexcept {
+			return _end - _start;
+		}
+		[[nodiscard]]
+		constexpr bool is_empty() const noexcept {
+			return !size();
+		}
+		[[nodiscard]]
+		constexpr array_view sub(size_t start, size_t n = -1) const noexcept {
+			return array_view(_start + start, n != -1 ? _start + n : _end);
+		}
+	};
+} // namespace libstra
---
+++ tests/views.cpp
@@ -0,0 +1,19 @@
+#include <libstra/views.hpp>
+#include <cassert>
+#include <iterator>
+
+int main(int argc, char const *argv[]) {
+	constexpr int X[] = { 0, 1, 2 };
+	libstra::array_view<int> v(std::begin(X), std::end(X));
+	assert(!v.is_empty());
+
+	for (size_t i = 0; i < v.size(); i++) {
+		assert(v[i] == X[i]);
+	}
+	auto s = v.sub(1);
+	assert(s.size() == 2 && s[0] == 1);
+	s = v.sub(0, 2);
+	assert(s.size() == 2 && s[0] == 0);
+
+	return 0;
+}
