7047ab42086aa201e0c36d9cfc4a9f98201282d5
EddieBreeg <eddiebreeg0@protonmail.com>
2023-08-09 14:47:38 +0200

    added a bunch of features to static_vector

--- include/libstra/static_vector.hpp
+++ include/libstra/static_vector.hpp
@@ -4,11 +4,17 @@
 #include <type_traits>
 #include <stdexcept>
 #include <libstra/utility.hpp>
+#include <iterator>
+#include "internal/config.h"
+#include <initializer_list>
 
 namespace libstra {
 
 	template <class T, size_t N>
 	class static_vector {
+		static_assert(!std::is_reference<T>::value,
+					  "T must not be a reference type");
+
 	private:
 		struct _storage {
 			alignas(T) char _buf[sizeof(T)];
@@ -17,7 +23,8 @@ namespace libstra {
 			const T *operator->() const { return (T *)_buf; }
 			T &operator*() { return *(T *)_buf; }
 			const T &operator*() const { return *(T *)_buf; }
-		} _elems[N];
+		};
+		_storage _elems[N];
 		size_t _size = 0;
 
 		template <bool Const>
@@ -36,7 +43,15 @@ namespace libstra {
 
 		public:
 			iterator_base(_Ptr_t p) : _ptr(p) {}
-			pointer operator->() noexcept { return (pointer)_ptr->_buf; }
+			[[nodiscard]]
+			pointer
+			operator->() noexcept {
+				return (pointer)_ptr;
+			}
+			[[nodiscard]]
+			pointer get() noexcept {
+				return (pointer)_ptr;
+			}
 			[[nodiscard]]
 			reference
 			operator*() noexcept {
@@ -85,21 +100,199 @@ namespace libstra {
 		using const_iterator = iterator_base<true>;
 		static_vector() = default;
 		static_vector(size_t n) : _size(n) {
+#ifndef NDEBUG
 			if (n > N)
-				throw std::out_of_range("Attempted to construct a static "
-										"vector with too many elements");
+				_unlikely throw std::out_of_range(
+					"Attempted to construct a static "
+					"vector with too many elements");
+#endif
 			for (size_t i = 0; i < n; i++)
 				new (_elems[i]._buf) T();
 		}
 		template <class U,
 				  std::enable_if_t<std::is_convertible<U, T>::value, int> = 0>
 		static_vector(size_t n, const U &val) : _size(n) {
+#ifndef NDEBUG
 			if (n > N)
-				throw std::out_of_range("Attempted to construct a static "
-										"vector with too many elements");
+				_unlikely throw std::out_of_range(
+					"Attempted to construct a static "
+					"vector with too many elements");
+#endif
 			for (size_t i = 0; i < n; i++)
 				new (_elems[i]._buf) T(val);
 		}
+		template <class Iter,
+				  // only enable if Iter is a valid iterator type
+				  typename std::iterator_traits<Iter>::difference_type = 0>
+		static_vector(Iter start, Iter end) : _size(std::distance(start, end)) {
+#ifndef NDEBUG
+			if (_size > N)
+				_unlikely throw std::out_of_range(
+					"Attempted to construct a static "
+					"vector with too many elements");
+#endif
+			for (auto p = _elems; start != end; ++start, ++p)
+				new (p->_buf) T(*start);
+		}
+		template <class U,
+				  std::enable_if_t<std::is_convertible<U, T>::value, int> = 0>
+		static_vector(std::initializer_list<U> il) : _size(il.size()) {
+#ifndef NDEBUG
+			if (_size > N)
+				_unlikely throw std::out_of_range(
+					"Attempted to construct a static "
+					"vector with too many elements");
+#endif
+			auto it = il.begin();
+			for (auto p = _elems; it != il.end(); ++it, ++p)
+				new (p->_buf) T(*it);
+		}
+
+		static_vector(const static_vector &other) {
+			_assume(other._size <= N);
+			for (; _size < other._size; ++_size)
+				new (_elems[_size]._buf) T(other[_size]);
+		}
+		static_vector(static_vector &&other) noexcept {
+			_assume(other._size <= N);
+			for (; _size < other._size; ++_size)
+				new (_elems[_size]._buf) T((T &&)(other[_size]));
+			other._size = 0;
+		}
+
+		static_vector &operator=(static_vector &&other) {
+			_assume(_size <= N && other._size <= N);
+			size_t m = _size < other._size ? _size : other._size;
+			for (size_t i = 0; i < m; ++i) {
+				std::swap(*(_elems[i]), *(other._elems[i]));
+			}
+			for (size_t i = m; i < other._size; ++i) {
+				*(_elems[i]) = (T &&)*(other._elems[i]);
+			}
+			for (size_t i = m; i < _size; ++i) {
+				*(other._elems[i]) = (T &&)*(_elems[i]);
+			}
+			std::swap(_size, other._size);
+			return *this;
+		}
+		static_vector &operator=(const static_vector &other) {
+			_assume(_size <= N && other._size <= N);
+			for (size_t i = 0; i < other._size; ++i)
+				*(_elems[i]) = *(other._elems[i]);
+			if (!std::is_trivially_destructible<T>::value) {
+				for (size_t i = other._size; i < _size; ++i)
+					_elems[i]->~T();
+			}
+			_size = other._size;
+			return *this;
+		}
+
+		template <class U>
+		iterator push_back(U &&val) {
+#ifndef NDEBUG
+			if (_size == N)
+				_unlikely throw std::out_of_range(
+					"Attempted to add an element to a full static vector");
+#endif
+			iterator it = end();
+			*(_elems[_size++]) = std::forward<U>(val);
+			return it;
+		}
+
+		template <class... Args>
+		iterator emplace_back(Args &&...args) {
+#ifndef NDEBUG
+			if (_size == N)
+				_unlikely throw std::out_of_range(
+					"Attempted to add an element to a full static vector");
+#endif
+			iterator it = end();
+			new (_elems[_size++]._buf) T(libstra::forward<Args>(args)...);
+			return it;
+		}
+		void pop_back() {
+#ifndef NDEBUG
+			if (!_size)
+				throw std::out_of_range(
+					"Called pop_back() on an empty static_vector");
+#endif
+			--_size;
+			if (!std::is_trivially_destructible<T>::value) _elems[_size]->~T();
+		}
+
+		[[nodiscard]]
+		T &front() & {
+#ifndef NDEBUG
+			if (!_size)
+				_unlikely throw std::out_of_range(
+					"Called front() on an empty static_vector");
+#endif
+			return *(_elems[0]);
+		}
+		[[nodiscard]]
+		const T &front() const & {
+#ifndef NDEBUG
+			if (!_size)
+				_unlikely throw std::out_of_range(
+					"Called front() on an empty static_vector");
+#endif
+			return *(_elems[0]);
+		}
+		[[nodiscard]]
+		T &&front() && {
+#ifndef NDEBUG
+			if (!_size)
+				_unlikely throw std::out_of_range(
+					"Called front() on an empty static_vector");
+#endif
+			return *(_elems[0]);
+		}
+		[[nodiscard]]
+		const T &&front() const && {
+#ifndef NDEBUG
+			if (!_size)
+				_unlikely throw std::out_of_range(
+					"Called front() on an empty static_vector");
+#endif
+			return *(_elems[0]);
+		}
+		[[nodiscard]]
+		T &back() & {
+#ifndef NDEBUG
+			if (!_size)
+				_unlikely throw std::out_of_range(
+					"Called back() on an empty static_vector");
+#endif
+			return *(_elems[_size - 1]);
+		}
+		[[nodiscard]]
+		const T &back() const & {
+#ifndef NDEBUG
+			if (!_size)
+				_unlikely throw std::out_of_range(
+					"Called back() on an empty static_vector");
+#endif
+			return *(_elems[_size - 1]);
+		}
+		[[nodiscard]]
+		T &&back() && {
+#ifndef NDEBUG
+			if (!_size)
+				_unlikely throw std::out_of_range(
+					"Called back() on an empty static_vector");
+#endif
+			return *(_elems[_size - 1]);
+		}
+		[[nodiscard]]
+		const T &&back() const && {
+#ifndef NDEBUG
+			if (!_size)
+				_unlikely throw std::out_of_range(
+					"Called back() on an empty static_vector");
+#endif
+			return *(_elems[_size - 1]);
+		}
+
 		[[nodiscard]]
 		iterator begin() noexcept {
 			return iterator(_elems);
@@ -110,12 +303,76 @@ namespace libstra {
 		}
 		[[nodiscard]]
 		iterator end() noexcept {
-			return iterator(_elems + N);
+			return iterator(_elems + _size);
 		}
 		[[nodiscard]]
 		const_iterator end() const noexcept {
-			return (_elems + N);
+			return (_elems + _size);
 		}
+
+		[[nodiscard]]
+		inline T &
+		operator[](size_t i) & {
+#ifndef NDEBUG
+			if (i >= _size)
+				_unlikely throw std::out_of_range(
+					"static_vector subscript out of range");
+#endif
+			return *(_elems[i]);
+		}
+		[[nodiscard]]
+		inline const T &
+		operator[](size_t i) const & {
+#ifndef NDEBUG
+			if (i >= _size)
+				_unlikely throw std::out_of_range(
+					"static_vector subscript out of range");
+#endif
+			return *(_elems[i]);
+		}
+		[[nodiscard]]
+		inline T &&
+		operator[](size_t i) && {
+#ifndef NDEBUG
+			if (i >= _size)
+				_unlikely throw std::out_of_range(
+					"static_vector subscript out of range");
+#endif
+			return *(_elems[i]);
+		}
+		[[nodiscard]]
+		inline const T &&
+		operator[](size_t i) const && {
+#ifndef NDEBUG
+			if (i >= _size)
+				_unlikely throw std::out_of_range(
+					"static_vector subscript out of range");
+#endif
+			return *(_elems[i]);
+		}
+
+		[[nodiscard]]
+		T *data() noexcept {
+			return (T *)_elems;
+		}
+		[[nodiscard]]
+		const T *data() const noexcept {
+			return (T *)_elems;
+		}
+
+		[[nodiscard]]
+		inline size_t size() const noexcept {
+			return _size;
+		}
+		[[nodiscard]]
+		inline bool is_empty() const noexcept {
+			return _size == 0;
+		}
+		[[nodiscard]]
+		static constexpr inline size_t capacity() noexcept {
+			return N;
+		}
+
 		~static_vector() {
 			if (std::is_trivially_destructible<T>::value) return;
 			for (size_t i = 0; i < _size; i++) {
--- tests/static_vector.cpp
+++ tests/static_vector.cpp
@@ -5,6 +5,16 @@
 struct Foo {
 	static int copies, instances, moves;
 	Foo() { ++instances; };
+	Foo(const Foo &) {
+		++copies;
+		++instances;
+	}
+	Foo(Foo &&) { ++moves; }
+	Foo &operator=(Foo &&) { return *this; }
+	Foo &operator=(const Foo &) {
+		++copies;
+		return *this;
+	}
 	~Foo() { --instances; }
 };
 int Foo::copies = 0, Foo::moves = 0, Foo::instances = 0;
@@ -13,16 +23,19 @@ int main(int argc, char const *argv[]) {
 	{
 		libstra::static_vector<Foo, 10> v;
 		assert(!Foo::instances);
+		assert(v.is_empty());
 	}
 	{
 		libstra::static_vector<Foo, 10> v(3);
 		assert(Foo::instances == 3);
 	}
+#ifndef NDEBUG
 	try {
 		libstra::static_vector<Foo, 2> v(3);
 		abort();
 	} catch (const std::out_of_range &) {
 	}
+#endif
 	{
 		const libstra::static_vector<int, 2> v(2, 'a');
 		for (int x : v) {
@@ -30,15 +43,83 @@ int main(int argc, char const *argv[]) {
 		}
 	}
 	{
-		libstra::static_vector<int, 3> v;
+		libstra::static_vector<int, 10> v(3, 42);
+		assert(v.size() == 3);
 		auto it = v.begin();
 		assert(it++ == v.begin());
 		assert(--it == v.begin());
 		assert(++it == (v.begin() + 1));
 		assert(it-- != (v.begin()));
-		assert(std::distance(v.begin(), v.end()) ==
-			   libstra::extent_v<decltype(v)>);
-	}
+		assert(std::distance(v.begin(), v.end()) == v.size());
 
+		auto v2 = v;
+		for (size_t i = 0; i < v.size(); i++) {
+			assert(v[i] == v2[i]);
+		}
+	}
+	{
+		libstra::static_vector<Foo, 10> v(5);
+		auto v2 = v;
+		assert(Foo::copies == v.size());
+		auto v3 = std::move(v);
+		assert(v.is_empty() && Foo::moves == v3.size());
+	}
 	assert(Foo::instances == 0);
+	{
+		Foo::moves = Foo::copies = 0;
+		libstra::static_vector<Foo, 10> v1(5);
+		decltype(v1) v2(2);
+		v2 = std::move(v1);
+		assert(v1.size() == 2 && v2.size() == 5);
+	}
+	{
+		libstra::static_vector<int, 10> v1(5, 42);
+		decltype(v1) v2(2);
+		v2 = v1;
+		assert(v1.size() == v2.size());
+		for (int x : v2)
+			assert(x == 42);
+	}
+	{
+		constexpr int X[] = { 0, 11, 666 };
+		libstra::static_vector<int, 10> v(std::begin(X), std::end(X));
+		constexpr auto N = libstra::extent_v<decltype(X)>;
+		assert(v.size() == N);
+		for (size_t i = 0; i < N; ++i)
+			assert(v[i] == X[i]);
+	}
+	{
+		libstra::static_vector<int, 10> v = { 0, 1, 2 };
+		assert(v.size() == 3);
+		for (size_t i = 0; i < 3; i++)
+			assert(v[i] == i);
+	}
+	{
+		libstra::static_vector<int, 2> v;
+		auto it = v.push_back(1ULL);
+		assert(v.size() == 1 && *it == 1);
+	}
+	{
+		struct A {
+			int val = 0;
+			A() = default;
+			A(int x) : val(x) {}
+			A(A &) = delete;
+			A(A &&other) : val(other.val) {}
+		};
+		libstra::static_vector<A, 5> v(1);
+		assert(v.front().val == 0);
+		v.emplace_back(42);
+		assert(v.back().val == 42);
+	}
+	{
+		Foo::instances = 0;
+		libstra::static_vector<Foo, 1> v(1);
+		v.pop_back();
+		assert(!Foo::instances);
+	}
+	{
+		libstra::static_vector<int, 3> v(3, 42);
+		assert(*v.data() == 42);
+	}
 }
