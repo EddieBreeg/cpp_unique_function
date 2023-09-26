50be82c95ab9580fb090845b0cecd435cf17ea73
EddieBreeg <eddiebreeg0@protonmail.com>
2023-09-26 17:49:47 +0200

    fixed includes in cmake config script

--- LibstraConfig.cmake
+++ LibstraConfig.cmake
@@ -11,6 +11,8 @@ set_target_properties(libstra::libstra PROPERTIES
 IMPORTED_LOCATION ${LIBSTRA_LIB_FILE}
 INCLUDE_DIRECTORIES ${CMAKE_CURRENT_LIST_DIR}/../../include
 )
+target_include_directories(libstra::libstra INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../../include)
+
 if(NOT DEFINED FIND_Libstra_QUIETLY)
 message("Found libstra at ${CMAKE_CURRENT_LIST_DIR}")    
 endif(NOT DEFINED FIND_Libstra_QUIETLY)
